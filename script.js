195757e3765db1db502893a7c926caa7134076f6
Gabriel Antônio <gabriel_as_lima@estudante.sesisenai.org.br>
2026-08-06 20:31:45 -0300

    1

--- script.js
+++ script.js
@@ -10,13 +10,14 @@
 //
 // Dica: use document.getElementById() e .textContent
 
-
+document.getElementById("btn1").addEventListener("click", function(){
+    document.getElementById("texto1").textContent = "Texto alterado com sucesso!"
+})
 
 // ---------- Exercicio 2 ----------
 // Ao clicar no botao, mude a cor do texto #texto2 para azul
 //
 // Dica: use .style.color = "blue"
-
 
 
 // ---------- Exercicio 3 ----------
